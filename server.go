1f7e1b4ae020e226fdea67e38794b3b9c91732e8
Stuart Scott <stuart.murray.scott@gmail.com>
2019-05-29 12:09:17 -0700

    Refactoring and add tests

--- server.go
+++ server.go
@@ -17,440 +17,266 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
-	"crypto/rsa"
 	"encoding/base64"
 	"github.com/AletheiaWareLLC/aliasgo"
+	"github.com/AletheiaWareLLC/aliasservergo"
 	"github.com/AletheiaWareLLC/bcgo"
 	"github.com/AletheiaWareLLC/bcnetgo"
 	"github.com/AletheiaWareLLC/colourgo"
-	"github.com/AletheiaWareLLC/financego"
-	"github.com/golang/protobuf/proto"
 	"html/template"
 	"image"
 	"image/color"
 	"image/draw"
 	"image/jpeg"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path"
+	"time"
 )
 
 func main() {
-	logFile, err := bcnetgo.SetupLogging()
+	rootDir, err := bcgo.GetRootDirectory()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	//log.Println("Root Dir:", rootDir)
+
+	logFile, err := bcgo.SetupLogging(rootDir)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 	defer logFile.Close()
+	//log.Println("Log File:", logFile.Name())
+
+	cacheDir, err := bcgo.GetCacheDirectory(rootDir)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	//log.Println("Cache Dir:", cacheDir)
+
+	cache, err := bcgo.NewFileCache(cacheDir)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	network := &bcgo.TcpNetwork{}
+
+	node, err := bcgo.GetNode(rootDir, cache, network)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	aliases := aliasgo.OpenAliasChannel()
+	if err := bcgo.LoadHead(aliases, cache, network); err != nil {
+		log.Println(err)
+	}
+	if err := bcgo.Pull(aliases, cache, network); err != nil {
+		log.Println(err)
+	}
+	node.AddChannel(aliases)
+
+	listener := &bcgo.PrintingMiningListener{os.Stdout}
 
 	// Serve Block Requests
-	go bcnetgo.Bind(bcgo.PORT_BLOCK, bcnetgo.HandleBlockPort)
+	go bcnetgo.Bind(bcgo.PORT_GET_BLOCK, bcnetgo.BlockPortHandler(cache, network))
 	// Serve Head Requests
-	go bcnetgo.Bind(bcgo.PORT_HEAD, bcnetgo.HandleHeadPort)
+	go bcnetgo.Bind(bcgo.PORT_GET_HEAD, bcnetgo.HeadPortHandler(cache, network))
 	// Serve Block Updates
-	// TODO only store blocks from registered customers in allowed channels (alias, file, meta, share, preview)
-	go bcnetgo.Bind(bcgo.PORT_CAST, bcnetgo.HandleCastPort)
+	go bcnetgo.Bind(bcgo.PORT_BROADCAST, bcnetgo.BroadcastPortHandler(cache, network, func(name string) (bcgo.Channel, error) {
+		return node.GetChannel(name)
+	}))
 
 	// Redirect HTTP Requests to HTTPS
 	go http.ListenAndServe(":80", http.HandlerFunc(bcnetgo.HTTPSRedirect))
 
 	// Serve Web Requests
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", bcnetgo.HandleStatic)
-	mux.HandleFunc("/alias", bcnetgo.HandleAlias)
-	mux.HandleFunc("/alias-register", bcnetgo.HandleAliasRegister)
-	mux.HandleFunc("/block", bcnetgo.HandleBlock)
-	mux.HandleFunc("/channel", bcnetgo.HandleChannel)
-	mux.HandleFunc("/canvas", HandleCanvas)
-	mux.HandleFunc("/stripe-webhook", HandleStripeWebhook)
-	mux.HandleFunc("/colour-register", HandleRegister)
-	mux.HandleFunc("/colour-subscribe", HandleSubscribe)
-	store, err := bcnetgo.GetSecurityStore()
+	mux.HandleFunc("/", bcnetgo.StaticHandler)
+	aliasTemplate, err := template.ParseFiles("html/template/alias.html")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	mux.HandleFunc("/alias", aliasservergo.AliasHandler(aliases, cache, network, aliasTemplate))
+	aliasRegistrationTemplate, err := template.ParseFiles("html/template/alias-register.html")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	mux.HandleFunc("/alias-register", aliasservergo.AliasRegistrationHandler(aliases, node, listener, aliasRegistrationTemplate))
+	blockTemplate, err := template.ParseFiles("html/template/block.html")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	mux.HandleFunc("/block", bcnetgo.BlockHandler(cache, network, blockTemplate))
+	channelTemplate, err := template.ParseFiles("html/template/channel.html")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	mux.HandleFunc("/channel", bcnetgo.ChannelHandler(cache, network, channelTemplate))
+	channelListTemplate, err := template.ParseFiles("html/template/channel-list.html")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	mux.HandleFunc("/channels", bcnetgo.ChannelListHandler(cache, network, channelListTemplate, node.GetChannels))
+	mux.HandleFunc("/keys", bcnetgo.KeyShareHandler(make(bcnetgo.KeyShareStore), 2*time.Minute))
+	mux.HandleFunc("/canvas", CanvasHandler(node))
+	mux.HandleFunc("/stripe-webhook", bcnetgo.StripeWebhookHandler)
+	registrationTemplate, err := template.ParseFiles("html/template/colour-register.html")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	publishableKey := os.Getenv("STRIPE_PUBLISHABLE_KEY")
+	mux.HandleFunc("/colour-register", bcnetgo.RegistrationHandler(aliases, node, listener, registrationTemplate, publishableKey))
+	subscriptionTemplate, err := template.ParseFiles("html/template/colour-subscribe.html")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	productId := os.Getenv("STRIPE_PRODUCT_ID")
+	planId := os.Getenv("STRIPE_PLAN_ID")
+	mux.HandleFunc("/colour-subscribe", bcnetgo.SubscriptionHandler(aliases, node, listener, subscriptionTemplate, "/subscribed.html", productId, planId))
+	certDir, err := bcgo.GetCertificateDirectory(rootDir)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 	// Serve HTTPS Requests
-	log.Println(http.ListenAndServeTLS(":443", path.Join(store, "fullchain.pem"), path.Join(store, "privkey.pem"), mux))
+	log.Println(http.ListenAndServeTLS(":443", path.Join(certDir, "fullchain.pem"), path.Join(certDir, "privkey.pem"), mux))
 }
 
-func HandleCanvas(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.RemoteAddr, r.Proto, r.Method, r.Host, r.URL.Path)
-	switch r.Method {
-	case "GET":
-		query := r.URL.Query()
-		var canvas string
-		if results, ok := query["canvas"]; ok && len(results) == 1 {
-			canvas = results[0]
-		}
-		log.Println("Canvas", canvas)
-		canvases, err := bcgo.OpenChannel(colourgo.COLOUR_PREFIX_CANVAS + colourgo.GetYear())
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		if len(canvas) > 0 {
-			hash, err := base64.RawURLEncoding.DecodeString(canvas)
-			if err != nil {
-				log.Println(err)
-				return
+func CanvasHandler(node *bcgo.Node) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.RemoteAddr, r.Proto, r.Method, r.Host, r.URL.Path)
+		switch r.Method {
+		case "GET":
+			query := r.URL.Query()
+			var canvas string
+			if results, ok := query["canvas"]; ok && len(results) == 1 {
+				canvas = results[0]
 			}
-			if err := colourgo.GetCanvas(canvases, "", nil, hash, func(entry *bcgo.BlockEntry, key []byte, c *colourgo.Canvas) error {
-				// TODO Draw image of canvas
-				i := image.NewRGBA(image.Rect(0, 0, int(c.Width), int(c.Height)))
-				blue := color.RGBA{0, 0, 255, 255}
-				draw.Draw(i, i.Bounds(), &image.Uniform{blue}, image.ZP, draw.Src)
-
-				var img image.Image = i
-				buffer := new(bytes.Buffer)
-				if err := jpeg.Encode(buffer, img, nil); err != nil {
-					return err
-				}
-
-				t, err := template.ParseFiles("html/template/canvas.html")
+			log.Println("Canvas", canvas)
+			if len(canvas) > 0 {
+				hash, err := base64.RawURLEncoding.DecodeString(canvas)
 				if err != nil {
-					return err
+					log.Println(err)
+					return
 				}
-				data := struct {
+				canvases := colourgo.OpenCanvasChannel()
+				if err := bcgo.LoadHead(canvases, node.Cache, node.Network); err != nil {
+					log.Println(err)
+				}
+				if err := bcgo.Pull(canvases, node.Cache, node.Network); err != nil {
+					log.Println(err)
+				}
+				if err := colourgo.GetCanvas(canvases, node.Cache, node.Network, "", nil, hash, func(entry *bcgo.BlockEntry, key []byte, c *colourgo.Canvas) error {
+					// TODO Draw image of canvas
+					i := image.NewRGBA(image.Rect(0, 0, int(c.Width), int(c.Height)))
+					blue := color.RGBA{0, 0, 255, 255}
+					draw.Draw(i, i.Bounds(), &image.Uniform{blue}, image.ZP, draw.Src)
+
+					var img image.Image = i
+					buffer := new(bytes.Buffer)
+					if err := jpeg.Encode(buffer, img, nil); err != nil {
+						return err
+					}
+
+					t, err := template.ParseFiles("html/template/canvas.html")
+					if err != nil {
+						return err
+					}
+					data := struct {
+						Hash      string
+						Timestamp string
+						Name      string
+						Width     uint32
+						Height    uint32
+						Depth     uint32
+						Mode      string
+						Image     string
+					}{
+						Hash:      canvas,
+						Timestamp: bcgo.TimestampToString(entry.Record.Timestamp),
+						Name:      c.Name,
+						Width:     c.Width,
+						Height:    c.Height,
+						Depth:     c.Depth,
+						Mode:      c.Mode.String(),
+						Image:     base64.StdEncoding.EncodeToString(buffer.Bytes()),
+					}
+					log.Println("Data", data)
+					err = t.Execute(w, data)
+					if err != nil {
+						return err
+					}
+					return nil
+				}); err != nil {
+					log.Println(err)
+					return
+				}
+			} else {
+				t, err := template.ParseFiles("html/template/canvas-list.html")
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				type TemplateCanvas struct {
 					Hash      string
-					Timestamp string
 					Name      string
+					Timestamp string
 					Width     uint32
 					Height    uint32
 					Depth     uint32
 					Mode      string
-					Image     string
+				}
+				cs := make([]TemplateCanvas, 0)
+				canvases := colourgo.OpenCanvasChannel()
+				if err := bcgo.LoadHead(canvases, node.Cache, node.Network); err != nil {
+					log.Println(err)
+				}
+				if err := bcgo.Pull(canvases, node.Cache, node.Network); err != nil {
+					log.Println(err)
+				}
+				if err := colourgo.GetCanvas(canvases, node.Cache, node.Network, "", nil, nil, func(entry *bcgo.BlockEntry, key []byte, c *colourgo.Canvas) error {
+					cs = append(cs, TemplateCanvas{
+						Hash:      base64.RawURLEncoding.EncodeToString(entry.RecordHash),
+						Timestamp: bcgo.TimestampToString(entry.Record.Timestamp),
+						Name:      c.Name,
+						Width:     c.Width,
+						Height:    c.Height,
+						Depth:     c.Depth,
+						Mode:      c.Mode.String(),
+					})
+					return nil
+				}); err != nil {
+					log.Println(err)
+					return
+				}
+				data := struct {
+					Canvas []TemplateCanvas
 				}{
-					Hash:      canvas,
-					Timestamp: bcgo.TimestampToString(entry.Record.Timestamp),
-					Name:      c.Name,
-					Width:     c.Width,
-					Height:    c.Height,
-					Depth:     c.Depth,
-					Mode:      c.Mode.String(),
-					Image:     base64.StdEncoding.EncodeToString(buffer.Bytes()),
+					Canvas: cs,
 				}
 				log.Println("Data", data)
 				err = t.Execute(w, data)
 				if err != nil {
-					return err
-				}
-				return nil
-			}); err != nil {
-				log.Println(err)
-				return
-			}
-		} else {
-			t, err := template.ParseFiles("html/template/canvas-list.html")
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			type TemplateCanvas struct {
-				Hash      string
-				Name      string
-				Timestamp string
-				Width     uint32
-				Height    uint32
-				Depth     uint32
-				Mode      string
-			}
-			cs := make([]TemplateCanvas, 0)
-			if err := colourgo.GetCanvas(canvases, "", nil, nil, func(entry *bcgo.BlockEntry, key []byte, c *colourgo.Canvas) error {
-				cs = append(cs, TemplateCanvas{
-					Hash:      base64.RawURLEncoding.EncodeToString(entry.RecordHash),
-					Timestamp: bcgo.TimestampToString(entry.Record.Timestamp),
-					Name:      c.Name,
-					Width:     c.Width,
-					Height:    c.Height,
-					Depth:     c.Depth,
-					Mode:      c.Mode.String(),
-				})
-				return nil
-			}); err != nil {
-				log.Println(err)
-				return
-			}
-			data := struct {
-				Canvas []TemplateCanvas
-			}{
-				Canvas: cs,
-			}
-			log.Println("Data", data)
-			err = t.Execute(w, data)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-		}
-	default:
-		log.Println("Unsupported method", r.Method)
-	}
-}
-
-func HandleStripeWebhook(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.RemoteAddr, r.Proto, r.Method, r.Host, r.URL.Path)
-	log.Println("Stripe Webhook", r)
-	data, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	event, err := financego.ConstructEvent(data, r.Header.Get("Stripe-Signature"))
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	log.Println("Event", event)
-	w.WriteHeader(http.StatusOK)
-}
-
-func HandleRegister(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.RemoteAddr, r.Proto, r.Method, r.Host, r.URL.Path)
-	switch r.Method {
-	case "GET":
-		query := r.URL.Query()
-		var alias string
-		if results, ok := query["alias"]; ok && len(results) == 1 {
-			alias = results[0]
-		}
-		log.Println("Alias", alias)
-		var publicKey string
-		if results, ok := query["publicKey"]; ok && len(results) == 1 {
-			publicKey = results[0]
-		}
-		log.Println("PublicKey", publicKey)
-		t, err := template.ParseFiles("html/template/colour-register.html")
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		data := struct {
-			Description string
-			Key         string
-			Name        string
-			Alias       string
-		}{
-			Description: "Colour",
-			Key:         os.Getenv("STRIPE_PUBLISHABLE_KEY"),
-			Name:        "Aletheia Ware LLC",
-			Alias:       alias,
-		}
-		log.Println("Data", data)
-		err = t.Execute(w, data)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-	case "POST":
-		r.ParseForm()
-		api := r.Form["api"]
-		alias := r.Form["alias"]
-		stripeEmail := r.Form["stripeEmail"]
-		// stripeBillingName := r.Form["stripeBillingName"]
-		// stripeBillingAddressLine1 := r.Form["stripeBillingAddressLine1"]
-		// stripeBillingAddressCity := r.Form["stripeBillingAddressCity"]
-		// stripeBillingAddressZip := r.Form["stripeBillingAddressZip"]
-		// stripeBillingAddressCountry := r.Form["stripeBillingAddressCountry"]
-		// stripeBillingAddressCountryCode := r.Form["stripeBillingAddressCountryCode"]
-		// stripeBillingAddressState := r.Form["stripeBillingAddressState"]
-		stripeToken := r.Form["stripeToken"]
-		// stripeTokenType := r.Form["stripeTokenType"]
-
-		if len(alias) > 0 && len(stripeEmail) > 0 && len(stripeToken) > 0 {
-			node, err := bcgo.GetNode()
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			aliases, err := aliasgo.OpenAliasChannel()
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			if err := aliases.Sync(); err != nil {
-				log.Println(err)
-				return
-			}
-			// Get rsa.PublicKey for Alias
-			publicKey, err := aliasgo.GetPublicKey(aliases, alias[0])
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			// Create list of access (user + server)
-			acl := map[string]*rsa.PublicKey{
-				alias[0]:   publicKey,
-				node.Alias: &node.Key.PublicKey,
-			}
-			log.Println("Access", acl)
-
-			stripeCustomer, bcCustomer, err := financego.NewCustomer(alias[0], stripeEmail[0], stripeToken[0], "Colour Customer")
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			log.Println("StripeCustomer", stripeCustomer)
-			log.Println("BcCustomer", bcCustomer)
-			customerData, err := proto.Marshal(bcCustomer)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			customers, err := financego.OpenCustomerChannel()
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			customerReference, err := node.Mine(customers, acl, nil, customerData)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			log.Println("CustomerReference", customerReference)
-
-			switch api[0] {
-			case "1":
-				w.Write([]byte(stripeCustomer.ID))
-				w.Write([]byte("\n"))
-			case "2":
-				if err := bcgo.WriteReference(bufio.NewWriter(w), customerReference); err != nil {
 					log.Println(err)
 					return
 				}
-			default:
-				http.Redirect(w, r, "/registered.html", http.StatusFound)
 			}
+		default:
+			log.Println("Unsupported method", r.Method)
 		}
-	default:
-		log.Println("Unsupported method", r.Method)
-	}
-}
-
-func HandleSubscribe(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.RemoteAddr, r.Proto, r.Method, r.Host, r.URL.Path)
-	switch r.Method {
-	case "GET":
-		query := r.URL.Query()
-		var alias string
-		if results, ok := query["alias"]; ok && len(results) == 1 {
-			alias = results[0]
-		}
-		log.Println("Alias", alias)
-		var id string
-		if results, ok := query["customerId"]; ok && len(results) == 1 {
-			id = results[0]
-		}
-		log.Println("Customer ID", id)
-		t, err := template.ParseFiles("html/template/colour-subscribe.html")
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		data := struct {
-			Alias      string
-			CustomerId string
-		}{
-			Alias:      alias,
-			CustomerId: id,
-		}
-		log.Println("Data", data)
-		err = t.Execute(w, data)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-	case "POST":
-		r.ParseForm()
-		api := r.Form["api"]
-		alias := r.Form["alias"]
-		customerId := r.Form["customerId"]
-
-		if len(alias) > 0 && len(customerId) > 0 {
-			node, err := bcgo.GetNode()
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			aliases, err := aliasgo.OpenAliasChannel()
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			if err := aliases.Sync(); err != nil {
-				log.Println(err)
-				return
-			}
-			// Get rsa.PublicKey for Alias
-			publicKey, err := aliasgo.GetPublicKey(aliases, alias[0])
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			// Create list of access (user + server)
-			acl := map[string]*rsa.PublicKey{
-				alias[0]:   publicKey,
-				node.Alias: &node.Key.PublicKey,
-			}
-			log.Println("Access", acl)
-
-			productId := os.Getenv("STRIPE_PRODUCT_ID")
-			planId := os.Getenv("STRIPE_PLAN_ID")
-
-			stripeSubscription, bcSubscription, err := financego.NewSubscription(alias[0], customerId[0], "", productId, planId)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			log.Println("StripeSubscription", stripeSubscription)
-			log.Println("BcSubscription", bcSubscription)
-
-			subscriptionData, err := proto.Marshal(bcSubscription)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			subscriptions, err := financego.OpenSubscriptionChannel()
-			if err != nil {
-				log.Println(err)
-				return
-			}
-
-			subscriptionReference, err := node.Mine(subscriptions, acl, nil, subscriptionData)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			log.Println("SubscriptionReference", subscriptionReference)
-
-			switch api[0] {
-			case "1":
-				w.Write([]byte(stripeSubscription.ID))
-				w.Write([]byte("\n"))
-			case "2":
-				if err := bcgo.WriteReference(bufio.NewWriter(w), subscriptionReference); err != nil {
-					log.Println(err)
-					return
-				}
-			default:
-				http.Redirect(w, r, "/subscribed.html", http.StatusFound)
-			}
-		}
-	default:
-		log.Println("Unsupported method", r.Method)
 	}
 }
